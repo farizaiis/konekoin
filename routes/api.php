2f6d0682ee708150dac83645346cccc3de39c54c
farizaiis <farizaiis@hotmail.com>
2022-04-14 14:35:03 +0700

    set auth and user api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\v1\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => ['auth:api']], function() {
+    //User Entity
+    Route::get('/v1/users', [UserController::class, 'index']);
+    Route::get('/v1/users/{id}', [UserController::class, 'show']);
+    Route::put('/v1/users/{id}', [UserController::class, 'update']);
 });
