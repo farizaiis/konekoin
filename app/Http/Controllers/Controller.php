ea431be29df3e98ac652fd669fa43f9a52df11c0
farizaiis <farizaiis@hotmail.com>
2022-04-15 17:31:13 +0700

    set swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,7 +10,7 @@ use Illuminate\Routing\Controller as BaseController;
 /**
      * @OA\Info(
      *      version="1.0.0",
-     *      title="Konekita Open API",
+     *      title="Konekoin Open API",
      * )
      */
 class Controller extends BaseController
