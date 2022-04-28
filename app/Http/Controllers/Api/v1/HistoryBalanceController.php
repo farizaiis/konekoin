1b9a79394b759c9d3e5044b7b6853105f8d43e06
farizaiis <farizaiis@hotmail.com>
2022-04-28 16:22:14 +0700

    fix date format for history balance

--- app/Http/Controllers/Api/v1/HistoryBalanceController.php
+++ app/Http/Controllers/Api/v1/HistoryBalanceController.php
@@ -227,7 +227,7 @@ class HistoryBalanceController extends BaseController
     {
         $input = $request->all();
 
-        $today = date('Y-m-d H:i');
+        $today = date('Y-m-d H:i:s');
 
         $validator = Validator::make($input, [
             'user_id' => ['integer'],
@@ -237,7 +237,7 @@ class HistoryBalanceController extends BaseController
             'amount' => ['integer'],
             'app_name' => ['string', 'in:Konekios,Konekita,konekios,konekita'],
             'status' => ['string', 'in:Menunggu,Berhasil,Ditolak,menunggu,berhasil,ditolak'],
-            'date' => ['string', 'date_format:Y-m-d H:i', 'after_or_equal:'.$today]
+            'date' => ['string', 'date_format:Y-m-d H:i:s', 'after_or_equal:'.$today]
         ]);
    
         if($validator->fails()){
